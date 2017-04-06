754e4a5f111a75a568db455fa7a78b6364d5f8a1
Nicholas Hansen <nicholasfhansen@gmail.com>
2017-04-06 10:19:28 +0200

    creating unit test project.

--- StorageService/DataAccess/StorageRepository.cs
+++ StorageService/DataAccess/StorageRepository.cs
@@ -1,8 +1,20 @@
 ﻿using System;
+using DomainTypes;
+using MongoDB.Driver;
 
 namespace DataAccess
 {
-    public class StorageRepository
+    public class StorageRepository : IStorageRepository
     {
+        private readonly IMongoClient _client;
+
+        public StorageRepository(IMongoClient client)
+        {
+            _client = client;
+        }
+        public void create(DataStore dataStore)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
