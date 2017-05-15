722d84b3d3ae725638e7419be27338af79c3fa01
Nicholas Hansen <nicholasfhansen@gmail.com>
2017-05-15 17:44:16 +0200

    changing mongodb connection string back and changing application url port to 5000

--- StorageService/EvilCorp.SlackStorage.StorageService.WebHost/Controllers/StorageController.cs
+++ StorageService/EvilCorp.SlackStorage.StorageService.WebHost/Controllers/StorageController.cs
@@ -24,7 +24,7 @@ namespace EvilCorp.SlackStorage.StorageService.WebHost.Controllers
             _logger = ConsoleFactory.CreateLogger();
 
             //Want to prevent hardcoding of the mongoClient IP.
-            _dataStoreRepo = new StorageRepository(new MongoClient("mongodb://127.0.0.1:32768"));
+            _dataStoreRepo = new StorageRepository(new MongoClient("mongodb://localhost:27017/"));
         }
 
         // GET: api/storage/<userId>
